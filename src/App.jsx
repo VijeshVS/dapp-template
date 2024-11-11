40dfbb136279fb186c77cab0e57570e17533464d
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-11-11 21:54:21 +0530

    contract interaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { useEffect, useState } from 'react';
-import './App.css'
-import { useWallet } from './hooks/useWallet';
-import Web3 from 'web3';
-import { sendTransaction } from './utils/sendTransaction';
+import "./App.css";
+import { useWallet } from "./hooks/useWallet";
+import CONTRACT_ABI from "./contracts/signup.json";
+const CONTRACT_ADDRESS = "0x6F11bd1966Bb1Ed686BE27871a2cfb76b0C9A198";
 
 function App() {
-  const [web3,account,loading] = useWallet();
+  const [web3, account, loading] = useWallet();
+  
+  async function handler(){
+    let contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+    // const methods = contract.methods;
+    // console.log('All contract methods:', Object.keys(methods));
+    // console.log(account);
+    // const details = await contract.methods.getUserDetails(account).call({
+    //   from: account,
+    //   gas: 500000  // Adjust gas limit based on your contract's needs
+    // });
+    // console.log(details)
 
-  return (
-    <div>
-      
-    </div>
-  )
+    const gasEstimate = await contract.methods.getUserDetails(account).estimateGas({
+      from: account
+    });
+
+    // const etherValue = web3.utils.fromWei(gasEstimate, 'gwei');
+    // console.log(etherValue)
+
+    const details = await contract.methods.getUserDetails(account).call({
+      from: account,
+      gas: gasEstimate
+    });
+
+    console.log(details);
+  }
+
+  return <div>
+    <button onClick={handler}>Get Details</button>
+  </div>;
 }
 
-export default App
+export default App;
